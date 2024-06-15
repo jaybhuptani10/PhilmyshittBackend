8f1e84038ef8896ac2c4cc6d2c08f4368025ff11
unknown <jaybhuptani1054@gmail.com>
2024-06-15 19:16:32 +0530

    new

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ import cors from 'cors';
 const app = express(); 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
-        credentials: true
+        origin: "*",
+        credentials: true,
+        methods: "GET, POST, PUT, DELETE, OPTIONS",
     
     }
 ));
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
