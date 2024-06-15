f47d5c14a865c840c4f2e6bdce8f78c95a7fb2ca
unknown <jaybhuptani1054@gmail.com>
2024-06-15 19:58:48 +0530

    chal ja plz

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express(); 
 app.use(cors(
     {
-        origin: "http://localhost:5173/",
+        origin: ["http://localhost:5173/","https://philmyyshitt.vercel.app/"]   ,
         credentials: true,
        
     
