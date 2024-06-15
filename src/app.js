2a3baa7d7479d2e4c0ba9bf4def3e124f84fedf5
unknown <jaybhuptani1054@gmail.com>
2024-06-15 19:37:04 +0530

    link

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 const app = express(); 
 app.use(cors(
     {
-        origin: "https://philmyshitt-backend.vercel.app/",
+        origin: "http://localhost:5173/",
         credentials: true,
         methods: "GET, POST, PUT, DELETE, OPTIONS",
     
