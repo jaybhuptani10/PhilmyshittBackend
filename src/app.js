d43f493c70fb16e581020dff2285ba123ad23b02
unknown <jaybhuptani1054@gmail.com>
2024-06-15 20:15:53 +0530

    ok

--- src/app.js
+++ src/app.js
@@ -2,18 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express(); 
-app.use(cors(
-    {
-        origin: ["https://philmyyshitt.vercel.app/","http://localhost:5173/"]  ,
-        credentials: true,
-       
-    
-    }
-));
 
-app.use(express.json({limit: "16kb"}));
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 //Routes
 import fetchRouter from './routes/fetch.routes.js';
