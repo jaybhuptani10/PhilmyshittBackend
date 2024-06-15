5f84a78eb459f0df93800102abf19430ab82fe35
unknown <jaybhuptani1054@gmail.com>
2024-06-15 20:18:40 +0530

    ok

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ["https://philmyyshitt.vercel.app/","http://localhost:5173/"],
+    origin: ["https://philmyyshitt.vercel.app", "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
