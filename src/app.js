985ca610dd1510a23103ebe32f406b415cec9dfa
unknown <jaybhuptani1054@gmail.com>
2024-06-18 21:42:23 +0530

    j

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ import cors from 'cors';
 const app = express(); 
 
 // CORS
-app.options('*', cors()) // include before other routes
+const corsOptions = {
+    origin: ['https://philmyyshitt.vercel.app'],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+    credentials: true,
+    enablePreflight: true
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
 
 // Other middleware
 app.use(express.json({ limit: "16kb" }));
