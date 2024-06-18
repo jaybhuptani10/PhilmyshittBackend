87f0e69e460af09e63bba10756530452f2e1d7b8
unknown <jaybhuptani1054@gmail.com>
2024-06-18 21:02:38 +0530

    hoja bhai

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ app.use(cors({
     origin: ["https://philmyyshitt.vercel.app", "http://localhost:5173"],
     optionsSuccessStatus: 200,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
+app.options('*', cors())
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
