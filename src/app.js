0161b522e68b18170fc790f7c847a326f02dfa8c
unknown <jaybhuptani1054@gmail.com>
2024-06-18 21:29:09 +0530

    ho ja

--- src/app.js
+++ src/app.js
@@ -5,13 +5,11 @@ import cors from 'cors';
 const app = express(); 
 
 // CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://philmyyshitt.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+app.use(cors({
+    origin:["https://philmyyshitt.vercel.app"],
+    methods:["POST","GET"],
+    credentials:[true]
+}))
 
 // Other middleware
 app.use(express.json({ limit: "16kb" }));
