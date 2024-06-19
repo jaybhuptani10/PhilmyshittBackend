2f8d3370897ac87024e36e4f2753c29bfffdd8d4
unknown <jaybhuptani1054@gmail.com>
2024-06-19 10:35:34 +0530

    na

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express(); 
 app.use(cors({
-    origin: ["https://philmyyshitt.vercel.app", "http://localhost:5173"],
+    origin: ["https://philmyyshitt.vercel.app/", "http://localhost:5173"],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
