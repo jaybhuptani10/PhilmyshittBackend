146cff393c73e29e634c2bee786242dc17ac5ab5
unknown <jaybhuptani1054@gmail.com>
2024-06-19 15:20:57 +0530

    search bar

--- src/controllers/fetchDataFromApi.js
+++ src/controllers/fetchDataFromApi.js
@@ -26,7 +26,55 @@ export const fetchDataFromApi = async (req, res) => {
 
 export const getMovieInfo = async (req, res) => {
   const { movie } = req.query; // Extract the movie type from query parameters
+ 
   const url = `https://api.themoviedb.org/3/search/${movie}`;
+  
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:` ${process.env.Authorization}`
+      }
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    // Send the fetched data as the response
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data' });
+  }
+};
+export const getSeriesInfo = async (req, res) => {
+  const { series } = req.query; // Extract the movie type from query parameters
+  
+  const url = `https://api.themoviedb.org/3/search/${series}`;
+  
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:` ${process.env.Authorization}`
+      }
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    // Send the fetched data as the response
+    res.status(200).json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred while fetching data' });
+  }
+};
+export const getSearchedMovie = async (req, res) => {
+  const { result } = req.query; // Extract the movie type from query parameters
+  const url = `https://api.themoviedb.org/3/search/${result}`;
   
   try {
     const options = {
--- src/routes/fetch.routes.js
+++ src/routes/fetch.routes.js
@@ -1,7 +1,9 @@
 import {Router} from 'express';
-import { fetchDataFromApi, getMovieInfo } from '../controllers/fetchDataFromApi.js';
+import { fetchDataFromApi, getMovieInfo, getSearchedMovie, getSeriesInfo } from '../controllers/fetchDataFromApi.js';
 
 const fetchRouter = Router();
 fetchRouter.route('/fetch').get(fetchDataFromApi);
-fetchRouter.route('/search').get(getMovieInfo);
+fetchRouter.route('/moviedetails').get(getMovieInfo);
+fetchRouter.route('/seriesdetails').get(getSeriesInfo);
+fetchRouter.route('/search').get(getSearchedMovie)
 export default fetchRouter;
