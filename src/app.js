c65482b3ad4c9be9b60118a26cf1cc61b2cbd77e
jaybhuptani10 <115783493+jaybhuptani10@users.noreply.github.com>
2024-06-20 19:08:50 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 
 const app = express(); 
 const corsOptions = {
-    origin: ["https://philmyyshitt.vercel.app", "http://localhost:5173"],
+    origin: ["https://cinemavault.vercel.app", "http://localhost:5173"],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
