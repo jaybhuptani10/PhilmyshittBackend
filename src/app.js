c3bf80ff9c0afd18638f75b8365eaab9677f49ed
unknown <jaybhuptani1054@gmail.com>
2025-04-03 12:28:09 +0530

    url change

--- src/app.js
+++ src/app.js
@@ -1,34 +1,33 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express(); 
+const app = express();
 const corsOptions = {
-    origin: ["https://cinemavault.vercel.app", "http://localhost:5173"],
-    optionsSuccessStatus: 200,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: ["https://cinevault-sepia.vercel.app/", "http://localhost:5173"],
+  optionsSuccessStatus: 200,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
+app.options("*", cors(corsOptions));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
 //Routes
-import fetchRouter from './routes/fetch.routes.js';
-import userRouter from './routes/user.routes.js';
+import fetchRouter from "./routes/fetch.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 //ROutes Decalarion
 
-app.use('/api', fetchRouter);
-app.use('/user',userRouter);
-app.use('/', (req, res) => {
-    res.json("Hell");
+app.use("/api", fetchRouter);
+app.use("/user", userRouter);
+app.use("/", (req, res) => {
+  res.json("Hell");
 });
 
 export default app;
