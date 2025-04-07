be1da4c97fbf418eed574d432976ae037e294569
unknown <jaybhuptani1054@gmail.com>
2025-04-07 10:36:07 +0530

    latest without cloudinary

--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 // src/index.js
-import express from 'express';
+import express from "express";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-import connectDB from './db/index.js';
+import connectDB from "./db/index.js";
 import app from "./app.js";
-dotenv.config({ path: './.env' });
-
-
-
-
+dotenv.config({ path: "./.env" });
+//new commit
 const startServer = async () => {
-    try {
-        await connectDB();
-        const PORT = process.env.PORT || 8000;
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error("Error starting server: ", error);
-        process.exit(1); // Exit with a failure code
-    }
+  try {
+    await connectDB();
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting server: ", error);
+    process.exit(1); // Exit with a failure code
+  }
 };
 
 startServer();
-
-
